7b411c6376e708bf775adc7ed17454b70a931dca
Darshil Kanzariya <darshilkanzariya8251@gmail.com>
2024-08-16 16:53:46 +0530

    fronted code

---
+++ src/pages/NotificationPage.js
@@ -0,0 +1,115 @@
+import React from "react";
+import Layout from "./../components/Layout";
+import { message, Tabs } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const NotificationPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.user);
+  //   handle read notification
+  const handleMarkAllRead = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/get-all-notification",
+        {
+          userId: user._id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        message.success(res.data.message);
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+      message.error("something went wrong");
+    }
+  };
+
+  // delete notifications
+  const handleDeleteAllRead = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/delete-all-notification",
+        { userId: user._id },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        message.success(res.data.message);
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+      message.error("Something went wrong in notifications");
+    }
+  };
+  return (
+    <Layout>
+      <h4 className="p-3 text-center">Notification Page</h4>
+      <Tabs>
+        <Tabs.TabPane tab="Unread Message" key={0}>
+          <div className="d-flex justify-content-end">
+          <button 
+          className="btn btn-primary" 
+          onClick={handleMarkAllRead}
+        >
+          Read
+        </button>
+          </div>
+          {user?.notifcation.map((notificationMgs) => (
+            <div className="card" style={{ cursor: "pointer" }}>
+              <div
+                className="card-text"
+                onClick={() => navigate(notificationMgs.onClickPath)}
+              >
+                {notificationMgs.message}
+              </div>
+            </div>
+          ))}
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Read message" key={1}>
+          <div className="d-flex justify-content-end">
+          <button 
+          className="btn btn-primary" 
+          onClick={handleDeleteAllRead}
+        >
+         Delete
+        </button>
+          </div>
+          {user?.seennotification.map((notificationMgs) => (
+            <div className="card" style={{ cursor: "pointer" }}>
+              <div
+                className="card-text"
+                onClick={() => navigate(notificationMgs.onClickPath)}
+              >
+                {notificationMgs.message}
+              </div>
+            </div>
+          ))}
+        </Tabs.TabPane>
+      </Tabs>
+    </Layout>
+  );
+};
+
+export default NotificationPage;
